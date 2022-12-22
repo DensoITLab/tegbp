2dbdb6383a37319f713351840b6f298b5d7741c1
jngt <nagata.jun@core.d-itlab.co.jp>
2022-12-22 18:32:16 +0900

    update preprocess

--- preprocess/create_normal_flow_event.m
+++ preprocess/create_normal_flow_event.m
@@ -6,11 +6,18 @@ clearvars
 
 addpath matlab/outer/flow-code-matlab/
 
+%%
+flg_save    = false;
+flg_movie   = true;
+framerate   = 30;
+batch       = 10^6;
+
 %% config (ymlにする?)
-win_x       = 2;        % spartial window size
+win_x       = 3;        % spartial window size
 win_t       = 40000;    % temporal window size
 t_rf        = 40000;    % refractory filter
-n_trial     = 2;        % number of trial for flane fitting
+dt_vis      = 50000;    % duration for visualize
+n_trial     = 3;        % number of trial for flane fitting
 n_th_inlier = 5;        % event number threshold foutlier rejection in plane fitting
 t_th_inlier = -1;       % temporal threshold foutlier rejection in plane fitting
 th_inlier   = 0.5;      % threshold inlier ratio
@@ -18,7 +25,7 @@ dt_scale    = 5000;     % dt scale
 
 data_dir    = '/home/jnagata/petaco/data/DNW/221108_EVK2_150mm_f8/500lux';
 data_name   = 'recording_2022-11-08_14-02-40';
-save_name   = 'qr000';
+save_name   = 'qr001';
 
 %% load data
 event_path  = fullfile(data_dir, [data_name, '_cd.mat']);
@@ -37,7 +44,7 @@ patchq      = int16(-win_x : win_x);
 
 %% visualize
 i_frame     = 1;
-batch       = 10^5;
+n_frame     = floor(n_event/batch);
 flow_norm   = zeros(h, w, 2);
 max_dist    = 100;
 max_flow    = sqrt(2);
@@ -52,23 +59,31 @@ for i = 1 : 2
     axis off
 end
 
+Frame(n_frame) = struct('cdata',[], 'colormap',[]);
+
 %% save normal flow
 event.vx_perp = nan(n_event, 1);
 event.vy_perp = nan(n_event, 1);
+
+%%
+t_skip      = 1000000;
+% t_skip      = 0;
+e_start     = find(event.t > t_skip, 1);
 
 %% event loop
 disp('start event loop')
 t_start     = tic;
 ts_start    = event.t(1);
-for e = 1 : n_event
+for e = e_start : n_event
     if rem(e, 100000) == 0 % 1M events
+        i_frame     = i_frame + 1;
         t_process   = toc(t_start);
-        t_actual    = event.t(e);
+        t_actual    = event.t(e) - ts_start;
         fprintf('%dK events: processing time %.2f s, actual time %.2f s\n', batch/1000, t_process, t_actual*10^-6);
         
-        mask = t_fit > (ti - win_t);
+        mask = t_fit > (ti - dt_vis);
 
-        I_sae = uint8((sae - (ti - win_t)) / win_t * 255); % 正規化
+        I_sae = uint8((sae - (ti - dt_vis)) / dt_vis * 255); % 正規化
         I_sae(I_sae < 0)    = 0;
         im{1}.CData         = I_sae;
 
@@ -78,8 +93,13 @@ for e = 1 : n_event
 
         drawnow
 
+        if flg_movie
+            Frame(i_frame) = getframe(fig);
+        end    
+
         %% next slide
         t_start     = tic;
+        ts_start    = event.t(e);
     end
 
     xi = event.x(e);
@@ -139,6 +159,19 @@ for e = 1 : n_event
 
 end
 
-save_normal_flow_event(event, save_name)
-
+if flg_movie
+    %%
+    Frame       = Frame(arrayfun(@(x) ~isempty(x.cdata), Frame)); % empty
+    movie_path  = fullfile('movie', 'normal_flow', save_name);
+    v           = VideoWriter(movie_path);
+    v.FrameRate = framerate;
+    open(v);
+    writeVideo(v, Frame);
+    close(v);
+    system(sprintf('ffmpeg -y -i %s.avi -vf scale=2000:-2 %s.mp4', movie_path, movie_path));
+end
+if flg_save
+    %%
+    save_normal_flow_event(event, save_name)
+end
 
